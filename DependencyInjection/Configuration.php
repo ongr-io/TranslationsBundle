5d5beb6b13b288961980b329f600d78bc0cb84f7
Linas Mockus <linas.mockus@ongr.io>
2015-02-09 12:34:40 +0200

    Initial commit.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\TranslationsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from app/config files.
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('ongr_translations');
+
+        return $treeBuilder;
+    }
+}
---
+++ README.rst
@@ -0,0 +1,8 @@
+===================
+Translations Bundle
+===================
+
+License
+~~~~~~~
+
+This bundle is under the MIT license. Please, see the complete license in the bundle `LICENSE </LICENSE>`_ file.
